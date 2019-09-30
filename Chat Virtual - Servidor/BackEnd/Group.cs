aad15536bf367914889be82f467e57b7df4932b7
jpreciadom <36993234+jpreciadom@users.noreply.github.com>
2019-09-29 20:23:47 -0500

    Uso de colas

---
+++ Chat Virtual - Servidor/BackEnd/Group.cs
@@ -0,0 +1,9 @@
+﻿using DataStructures;
+
+namespace Chat_Virtual___Servidor {
+    public class Group {
+        public string Name { get; set; }
+        public LinkedQueue<User> Users { get; set; }
+        
+    }
+}
