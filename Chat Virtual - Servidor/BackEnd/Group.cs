b94e5958da399755c5c256a56d48cd00c64665cc
jpreciadom <36993234+jpreciadom@users.noreply.github.com>
2019-09-29 21:27:09 -0500

    Revert "Uso de colas"
    This reverts commit aad15536bf367914889be82f467e57b7df4932b7.

--- Chat Virtual - Servidor/BackEnd/Group.cs
+++
@@ -1,9 +0,0 @@
-﻿using DataStructures;
-
-namespace Chat_Virtual___Servidor {
-    public class Group {
-        public string Name { get; set; }
-        public LinkedQueue<User> Users { get; set; }
-        
-    }
-}
