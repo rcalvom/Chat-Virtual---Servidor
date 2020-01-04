cfcb45159e5110a872e325e6593411b464e56ba9
jpreciadom <36993234+jpreciadom@users.noreply.github.com>
2020-01-04 11:16:37 -0500

    Arreglos

--- Chat Virtual - Servidor/Connection/ServerConnection.cs
+++ Chat Virtual - Servidor/Connection/ServerConnection.cs
@@ -161,7 +161,19 @@ namespace Chat_Virtual___Servidor {
                     if (Readed == null) {
                         continue;
                     } else if (Readed is Chat ch) {                                                             // Si se desea obtener la información de un Chat privado.
-
+                        if (ch.Searched) {
+                            this.Oracle.Oracle.ExecuteSQL(
+                                "SELECT USUARIO, ESTADO, RUTA_FOTO" +
+                                "FROM USUARIOS" +
+                                "WHERE LOWER(USUARIO) LIKE '%" + ch.memberTwo.Name.ToLower() + "%'");
+                            while (this.Oracle.Oracle.DataReader.Read()) {
+                                string name = this.Oracle.Oracle.DataReader["USUARIO"].ToString();
+                                byte[] image = Serializer.SerializeImage(Image.FromFile(this.Oracle.Oracle.DataReader["RUTA_FOTO"].ToString()));
+                                string status = this.Oracle.Oracle.DataReader["ESTADO"].ToString();
+                                Profile profile = new Profile(name, image, status);
+                                user.WritingEnqueue(new Chat(ch.memberOne, profile, true));
+                            }
+                        }
                     } else if (Readed is ChatMessage ms) {                                                      // Si se envía un mensaje en privado.
 
                     } else if (Readed is ChatGroup group) {                                                     // Si se desea obtener la información de un grupo.
